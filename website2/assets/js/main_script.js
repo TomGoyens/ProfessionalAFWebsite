72c4d1be7a01e81fc79e179eaf472c9a6b99df08
tom@tom-HP-EliteBook-8570p <tom.goyens@gmail.com>
2018-12-14 16:52:51 +0100

    een hele hoop schijt

--- website2/assets/js/main_script.js
+++ website2/assets/js/main_script.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {stickyhead()};
+window.onscroll = function() {stickyhead();};
 
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
@@ -17,4 +17,66 @@ menuToggle.addEventListener("click", function(){
      for(var activated = 0; activated < activeElements.length; activated++){
           activeElements[activated].classList.toggle("active");
      }
-})
+});
+
+var loader = document.querySelector(".load-btn");
+var active2Elements = document.querySelectorAll(".active-2-element");
+loader.addEventListener("click", function(){
+     for(var activated = 0; activated < active2Elements.length; activated++){
+          active2Elements[activated].classList.toggle("active");
+     }
+});
+
+var ezLoader = document.querySelector(".ezLoader");
+ezLoader.addEventListener('click', ezLoadStart);
+
+function ezLoadStart(){
+  ezLoader.children[0].classList.toggle("active");
+}
+
+
+function addToToDo(){
+  var toDoListElement = document.createElement("li");
+  toDoListElement.classList.add("toDoListItem");
+  var toDoListElementClose = document.createElement("span");
+  var toDoInput = document.querySelector(".inputToDo").value;
+  console.log(toDoInput);
+  toDoListElement.innerHTML = toDoInput;
+  toDoListElementClose.innerHTML = "\u00D7";
+  var theToDoList = document.querySelector(".toDoList");
+  toDoListElement.appendChild(toDoListElementClose);
+  theToDoList.appendChild(toDoListElement);
+
+}
+
+function checkToDo(){
+  if (this.target.tagName === 'LI') {
+    this.target.classList.toggle('checked');
+  }
+}
+
+var addToDoBtn = document.querySelector(".addToDoBtn");
+addToDoBtn.addEventListener ('click', addToToDo);
+
+/*
+function checkToDo(){
+  if (this.target.tagName === 'LI') {
+    this.target.classList.toggle('checked');
+  }
+}
+var list = document.querySelector('.toDoList');
+list.addEventListener('click', checkToDo, false);
+*/
+var list = document.querySelector('.toDoList');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+/*to use:
+            innerHTML
+            queryselector
+            createElement("li")
+
+            */
